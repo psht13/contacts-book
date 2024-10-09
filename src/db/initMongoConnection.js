d7d404a383e6457c72f841170263b338f7400b96
Pavlo Yurchenko <52372416+psht13@users.noreply.github.com>
2024-10-09 16:27:30 +0300

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,9 +4,15 @@ import { env } from '../utils/env.util.js';
 
 export const initMongoConnection = async () => {
   try {
-    const uri = env('MONGODB_CONNECTION_URI');
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
 
-    await mongoose.connect(uri);
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    
     console.log('Mongo connection successfully established!');
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
